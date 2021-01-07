e3d36453812da43f33e3be52393e85f99aded58b
zkq <en09.007@gmail.com>
2021-01-06 16:42:29 -0800

    add svgd. See cifar_vggbc_svgd.py

--- svgd_base.py
+++ svgd_base.py
@@ -3,9 +3,10 @@ import torch.distributions as td
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.spatial.distance import pdist, squareform
+from copy import deepcopy
 
 class SVGD:
-    def __init__(self,particles_shape):
+    def __init__(self, particles_shape):
         self.historical_grad_square = torch.zeros(particles_shape)
         self.iter=0
 
@@ -26,7 +27,7 @@ class SVGD:
 
     def update(self, x0, grad_x0, stepsize=1e-3, bandwidth=-1, alpha=0.9, debug=False):
 
-        x = x0.clone().detach()
+        x = x0
 
         # adagrad with momentum
         eps_factor = 1e-8
@@ -43,6 +44,80 @@ class SVGD:
         self.iter+=1
 
         return x
+    
+    
+class SVGD_sampler:
+    def __init__(self, Net, num_sample):
+        
+        self.models = [Net() for _ in range(num_sample)]
+        num_paras = 0
+        for p in self.models[0].parameters():
+            if p.requires_grad:
+                num_paras += p.numel()
+            
+        self.params = torch.zeros(num_sample, num_paras)
+        self.grads = torch.zeros(num_sample, num_paras)
+        self.svgd = SVGD((num_sample, num_paras))
+        
+    def to(self, device):
+        self.models = [m.to(device) for m in self.models]
+        self.params = self.params.to(device)
+        self.grads = self.grads.to(device)
+        self.svgd.historical_grad_square = self.svgd.historical_grad_square.to(device)
+        return self
+        
+    def _model_to_params(self):
+        for (i, model) in enumerate(self.models):
+            offset = 0
+            for p in model.parameters():
+                if p.requires_grad:
+                    self.params[i, offset : offset + p.numel()].copy_(p.data.view(-1))
+                    self.grads[i, offset : offset + p.numel()].copy_(p.grad.view(-1))
+                
+            offset += p.numel()
+                
+    def _params_to_model(self):
+        for (i, model) in enumerate(self.models):
+            offset = 0
+            for p in model.parameters():
+                if p.requires_grad:
+                    p.data.view(-1).copy_(self.params[i, offset : offset + p.numel()])
+                
+            offset += p.numel()
+            
+    def step(self, *args, **kwargs):
+        self._model_to_params()
+        self.svgd.update(self.params, self.grads, *args, **kwargs)
+        self._params_to_model()
+        
+    def getloss(self, data, target, criterian):
+        loss = 0
+        for model in self.models:
+            output = model(data)
+            loss += criterian(output, target)
+            
+        return loss
+        
+    def zero_grad(self):
+        for (i, model) in enumerate(self.models):
+             model.zero_grad()
+             
+    def train(self):
+        for (i, model) in enumerate(self.models):
+             model.train()
+             
+    def eval(self):
+        for (i, model) in enumerate(self.models):
+             model.eval()
+        
+        
+                
+            
+            
+            
+        
+            
+        
 
 """
 test_dist = td.MultivariateNormal(torch.tensor([0.0,0.0]),torch.eye(2))
